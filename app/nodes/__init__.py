1bacb4df0840d4c7585eed8aa4b279eba120802a
annaundflora <clemenshamann@gmail.com>
2025-03-02 20:57:57 +0100

    Fix imports in nodes/__init__.py to use combined node

--- app/nodes/__init__.py
+++ app/nodes/__init__.py
@@ -2,14 +2,8 @@
 Extraction nodes for the ShipmentBot application.
 """
 
-from app.nodes.pickup_address_node import extract_pickup_address
-from app.nodes.delivery_address_node import extract_delivery_address
-from app.nodes.billing_address_node import extract_billing_address
-from app.nodes.shipment_node import extract_shipment
+from app.nodes.shipment_booking_node import extract_shipment_booking
 
 __all__ = [
-    "extract_pickup_address",
-    "extract_delivery_address",
-    "extract_billing_address",
-    "extract_shipment",
+    "extract_shipment_booking",
 ] 
